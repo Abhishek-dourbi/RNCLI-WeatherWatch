c4a6167fc1e8948b2eafc055103b4e3837dbf955
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2024-09-08 23:43:01 +0530

    write geocoding api file

---
+++ src/api/geocoding/@types/index.d.ts
@@ -0,0 +1,6 @@
+export interface GeocodingResponse {
+    id: number;
+    name: string;
+    latitude: number;
+    longitude: number;
+}
---
+++ src/api/geocoding/index.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { GeocodingResponse } from "./@types";
+
+export const getLocation = async (query: string): Promise<GeocodingResponse[]> => {
+    try {
+        const res = await axios.get(
+            'https://geocoding-api.open-meteo.com/v1/search',
+            {
+                params: {
+                    name: query
+                }
+            }
+        );
+
+        return res?.data?.results;
+    } catch(err: any) {
+        throw new Error(err.message || 'Something went wrong');
+    }
+  };
+  
