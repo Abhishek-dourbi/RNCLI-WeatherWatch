63324cb27d5e37763d78aba39be43b042ab9df04
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2024-09-09 01:44:05 +0530

    update color theme

--- src/themes/colors.ts
+++ src/themes/colors.ts
@@ -2,7 +2,8 @@ const Colors = {
     blue: '#20A7DB',
     darkBlue: '#1c96c5',
     white: '#fff',
-    grey: '#d3d3d3'
+    grey: '#d3d3d3',
+    overlayBlack: 'rgba(0,0,0,0.7)',
 }
 
 export default Colors;
