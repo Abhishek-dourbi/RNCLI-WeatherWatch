543c156ba263c824ee72998a323d5458bc697579
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2024-09-09 23:58:44 +0530

    modify tests of getWeatherImage function

--- src/helpers/__tests__/getWeatherImage.test.ts
+++ src/helpers/__tests__/getWeatherImage.test.ts
@@ -1,37 +1,154 @@
-import getWeatherImage from '../getWeatherImage';
+import getWeatherImage, {WeatherCode} from '../getWeatherImage';
+interface TestData {
+  weatherCode: WeatherCode;
+  image: string;
+  description: string;
+}
 
 test.each([
-  {weatherCode: '0', image: 'https://openweathermap.org/img/wn/01d@2x.png'},
-  {weatherCode: '1', image: 'https://openweathermap.org/img/wn/01d@2x.png'},
-  {weatherCode: '2', image: 'https://openweathermap.org/img/wn/02d@2x.png'},
-  {weatherCode: '3', image: 'https://openweathermap.org/img/wn/03d@2x.png'},
-  {weatherCode: '45', image: 'https://openweathermap.org/img/wn/50d@2x.png'},
-  {weatherCode: '48', image: 'https://openweathermap.org/img/wn/50d@2x.png'},
-  {weatherCode: '51', image: 'https://openweathermap.org/img/wn/09d@2x.png'},
-  {weatherCode: '53', image: 'https://openweathermap.org/img/wn/09d@2x.png'},
-  {weatherCode: '55', image: 'https://openweathermap.org/img/wn/09d@2x.png'},
-  {weatherCode: '56', image: 'https://openweathermap.org/img/wn/09d@2x.png'},
-  {weatherCode: '57', image: 'https://openweathermap.org/img/wn/09d@2x.png'},
-  {weatherCode: '61', image: 'https://openweathermap.org/img/wn/10d@2x.png'},
-  {weatherCode: '63', image: 'https://openweathermap.org/img/wn/10d@2x.png'},
-  {weatherCode: '65', image: 'https://openweathermap.org/img/wn/10d@2x.png'},
-  {weatherCode: '66', image: 'https://openweathermap.org/img/wn/10d@2x.png'},
-  {weatherCode: '67', image: 'https://openweathermap.org/img/wn/10d@2x.png'},
-  {weatherCode: '71', image: 'https://openweathermap.org/img/wn/13d@2x.png'},
-  {weatherCode: '73', image: 'https://openweathermap.org/img/wn/13d@2x.png'},
-  {weatherCode: '75', image: 'https://openweathermap.org/img/wn/13d@2x.png'},
-  {weatherCode: '77', image: 'https://openweathermap.org/img/wn/13d@2x.png'},
-  {weatherCode: '80', image: 'https://openweathermap.org/img/wn/09d@2x.png'},
-  {weatherCode: '81', image: 'https://openweathermap.org/img/wn/09d@2x.png'},
-  {weatherCode: '82', image: 'https://openweathermap.org/img/wn/09d@2x.png'},
-  {weatherCode: '85', image: 'https://openweathermap.org/img/wn/13d@2x.png'},
-  {weatherCode: '86', image: 'https://openweathermap.org/img/wn/13d@2x.png'},
-  {weatherCode: '95', image: 'https://openweathermap.org/img/wn/11d@2x.png'},
-  {weatherCode: '96', image: 'https://openweathermap.org/img/wn/11d@2x.png'},
-  {weatherCode: '99', image: 'https://openweathermap.org/img/wn/11d@2x.png'},
-])(
+  {
+    weatherCode: '0',
+    description: 'Sunny',
+    image: 'https://openweathermap.org/img/wn/01d@2x.png',
+  },
+  {
+    weatherCode: '1',
+    description: 'Mainly Sunny',
+    image: 'https://openweathermap.org/img/wn/01d@2x.png',
+  },
+  {
+    weatherCode: '2',
+    description: 'Partly Cloudy',
+    image: 'https://openweathermap.org/img/wn/02d@2x.png',
+  },
+  {
+    weatherCode: '3',
+    description: 'Cloudy',
+    image: 'https://openweathermap.org/img/wn/03d@2x.png',
+  },
+  {
+    weatherCode: '45',
+    description: 'Foggy',
+    image: 'https://openweathermap.org/img/wn/50d@2x.png',
+  },
+  {
+    weatherCode: '48',
+    description: 'Rime Fog',
+    image: 'https://openweathermap.org/img/wn/50d@2x.png',
+  },
+  {
+    weatherCode: '51',
+    description: 'Light Drizzle',
+    image: 'https://openweathermap.org/img/wn/09d@2x.png',
+  },
+  {
+    weatherCode: '53',
+    description: 'Drizzle',
+    image: 'https://openweathermap.org/img/wn/09d@2x.png',
+  },
+  {
+    weatherCode: '55',
+    description: 'Heavy Drizzle',
+    image: 'https://openweathermap.org/img/wn/09d@2x.png',
+  },
+  {
+    weatherCode: '56',
+    description: 'Light Freezing Drizzle',
+    image: 'https://openweathermap.org/img/wn/09d@2x.png',
+  },
+  {
+    weatherCode: '57',
+    description: 'Freezing Drizzle',
+    image: 'https://openweathermap.org/img/wn/09d@2x.png',
+  },
+  {
+    weatherCode: '61',
+    description: 'Light Rain',
+    image: 'https://openweathermap.org/img/wn/10d@2x.png',
+  },
+  {
+    weatherCode: '63',
+    description: 'Rain',
+    image: 'https://openweathermap.org/img/wn/10d@2x.png',
+  },
+  {
+    weatherCode: '65',
+    description: 'Heavy Rain',
+    image: 'https://openweathermap.org/img/wn/10d@2x.png',
+  },
+  {
+    weatherCode: '66',
+    description: 'Light Freezing Rain',
+    image: 'https://openweathermap.org/img/wn/10d@2x.png',
+  },
+  {
+    weatherCode: '67',
+    description: 'Freezing Rain',
+    image: 'https://openweathermap.org/img/wn/10d@2x.png',
+  },
+  {
+    weatherCode: '71',
+    description: 'Light Snow',
+    image: 'https://openweathermap.org/img/wn/13d@2x.png',
+  },
+  {
+    weatherCode: '73',
+    description: 'Snow',
+    image: 'https://openweathermap.org/img/wn/13d@2x.png',
+  },
+  {
+    weatherCode: '75',
+    description: 'Heavy Snow',
+    image: 'https://openweathermap.org/img/wn/13d@2x.png',
+  },
+  {
+    weatherCode: '77',
+    description: 'Snow Grains',
+    image: 'https://openweathermap.org/img/wn/13d@2x.png',
+  },
+  {
+    weatherCode: '80',
+    description: 'Light Showers',
+    image: 'https://openweathermap.org/img/wn/09d@2x.png',
+  },
+  {
+    weatherCode: '81',
+    description: 'Showers',
+    image: 'https://openweathermap.org/img/wn/09d@2x.png',
+  },
+  {
+    weatherCode: '82',
+    description: 'Heavy Showers',
+    image: 'https://openweathermap.org/img/wn/09d@2x.png',
+  },
+  {
+    weatherCode: '85',
+    description: 'Light Snow Showers',
+    image: 'https://openweathermap.org/img/wn/13d@2x.png',
+  },
+  {
+    weatherCode: '86',
+    description: 'Snow Showers',
+    image: 'https://openweathermap.org/img/wn/13d@2x.png',
+  },
+  {
+    weatherCode: '95',
+    description: 'Thunderstorm',
+    image: 'https://openweathermap.org/img/wn/11d@2x.png',
+  },
+  {
+    weatherCode: '96',
+    description: 'Light Thunderstorms With Hail',
+    image: 'https://openweathermap.org/img/wn/11d@2x.png',
+  },
+  {
+    weatherCode: '99',
+    description: 'Thunderstorm With Hail',
+    image: 'https://openweathermap.org/img/wn/11d@2x.png',
+  },
+] as TestData[])(
   'should render the correct image for weather code: $weatherCode',
-  ({weatherCode, image}) => {
-    expect(getWeatherImage(weatherCode)).toEqual(image);
+  ({weatherCode, image, description}) => {
+    expect(getWeatherImage(weatherCode)).toEqual({description, image});
   },
 );
